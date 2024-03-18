9855484ebc31c00221a5100cec3a91089da99af6
kburaky <kburak.ylmz@gmail.com>
2024-03-18 23:40:00 +0300

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
-import React from 'react'
-import Index from './pages/Index'
-import About from './pages/About'
-import BreakfastFoods from './pages/BreakfastFoods'
-import PageNotFound from './pages/PageNotFound'
-import ModelBilesenBir from './pages/ModelBilesenBir'
-import ModelBilesenIki from './pages/ModelBilesenIki'
-import ModelBilesenUc from './pages/ModelBilesenUc'
-import ModelBilesenDort from './pages/ModelBilesenDort'
-import ModelBilesenBes from './pages/ModelBilesenBes'
-import ModelBilesenAlti from './pages/ModelBilesenAlti'
-import ModelBilesenYedi from './pages/ModelBilesenYedi'
-import ModelBilesenSekiz from './pages/ModelBilesenSekiz'
-import ModelBilesenDokuz from './pages/ModelBilesenDokuz'
-import {BrowserRouter,Routes, Route} from 'react-router-dom'
+import React from "react"
+import Index from "./pages/Index"
+import About from "./pages/About"
+import BreakfastFoods from "./pages/BreakfastFoods"
+import PageNotFound from "./pages/PageNotFound"
+import ModelBilesenBir from "./pages/ModelBilesenBir"
+import ModelBilesenIki from "./pages/ModelBilesenIki"
+import ModelBilesenUc from "./pages/ModelBilesenUc"
+import ModelBilesenDort from "./pages/ModelBilesenDort"
+import ModelBilesenBes from "./pages/ModelBilesenBes"
+import ModelBilesenAlti from "./pages/ModelBilesenAlti"
+import ModelBilesenYedi from "./pages/ModelBilesenYedi"
+import ModelBilesenSekiz from "./pages/ModelBilesenSekiz"
+import ModelBilesenDokuz from "./pages/ModelBilesenDokuz"
+import {BrowserRouter,Routes, Route} from "react-router-dom"
 
 
 function App() {
   return (
-    <div>
-    
+
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Index/>} />
-        <Route path='/hakkinda' element={<About/>} />
-        <Route path='/kahvalti-besinleri' element={<BreakfastFoods/>} />
+        <Route path="/" element={<Index/>} />
+        <Route path="/hakkinda" element={<About/>} />
+        <Route path="/kahvalti-besinleri" element={<BreakfastFoods/>} />
         <Route path="/onceki-ilgili-davranis" element={<ModelBilesenBir/>} />
         <Route path="/algilanan-fayda" element={<ModelBilesenIki/>} />
         <Route path="/algilanan-engeller" element={<ModelBilesenUc/>} />
@@ -37,7 +36,7 @@ function App() {
       </Routes>
     </BrowserRouter>
     
-    </div>
+    
   )
 }
 
