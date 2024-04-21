94c739b440cdba9b454d53c40d06f81f68a1ddf7
kburaky <kburak.ylmz@gmail.com>
2024-04-22 00:04:44 +0300

    modified:   src/components/Product.jsx

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -6,17 +6,40 @@ function Product() {
         <div className="container-xxl py-5">
         <div className="container">
             <div className="row g-0 gx-5 align-items-end">
-                <div className="col-lg-6">
+                <div className="col-lg-4">
                     <div className="section-header text-start mb-5 wow fadeInUp" 
                     data-wow-delay="0.1s" 
                     style={{maxWidth: 500}}>
-                        <h1 className="display-5 mb-3">Our Products</h1>
-                        <p>Tempor ut dolore lorem kasd vero ipsum sit eirmod sit. Ipsum diam justo sed rebum vero dolor duo.</p>
+                        <h1 className="display-5 mb-3">Besinler</h1>
+                        <p>Kahvaltı Besinleri ve kalori değerleri</p>
                     </div>
                 </div>
                 
+                <div class="col-lg-8 text-start text-lg-end wow slideInRight" data-wow-delay="0.1s">
+                    <ul class="nav nav-pills d-inline-flex justify-content-end mb-5">
+                        <li class="nav-item me-2">
+                            <a class="btn btn-outline-primary border-2 active" data-bs-toggle="pill" href="#tab-1">Süt ve Süt Ürünleri</a>
+                        </li>
+                        <li class="nav-item me-2">
+                            <a class="btn btn-outline-primary border-2" data-bs-toggle="pill" href="#tab-2">Meyveler </a>
+                        </li>
+                        <li class="nav-item me-2">
+                            <a class="btn btn-outline-primary border-2" data-bs-toggle="pill" href="#tab-3">Sebzeler</a>
+                        </li>
+                        <li class="nav-item me-2">
+                            <a class="btn btn-outline-primary border-2" data-bs-toggle="pill" href="#tab-4">Ekmekler ve Tahıllar</a>
+                        </li>
+                        <li class="nav-item me-0">
+                            <a class="btn btn-outline-primary border-2" data-bs-toggle="pill" href="#tab-5">Diğer</a>
+                        </li>
+                    </ul>
+                </div>
+
+
+
             </div>
             <div className="tab-content">
+
                 <div id="tab-1" className="tab-pane fade show p-0 active">
                     <div className="row g-4">
                         <div className="col-xl-3 col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
@@ -43,7 +66,7 @@ function Product() {
                         <div className="col-xl-3 col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
                             <div className="product-item">
                                 <div className="position-relative bg-light overflow-hidden">
-                                    <img className="img-fluid w-100" src="img/product-2.jpg" alt=""/>
+                                    <img className="img-fluid w-100" src="img/product-1.jpg" alt=""/>
                                     <div className="bg-secondary rounded text-white position-absolute start-0 top-0 m-4 py-1 px-3">New</div>
                                 </div>
                                 <div className="text-center p-4">
@@ -64,7 +87,7 @@ function Product() {
                         <div className="col-xl-3 col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
                             <div className="product-item">
                                 <div className="position-relative bg-light overflow-hidden">
-                                    <img className="img-fluid w-100" src="img/product-3.jpg" alt=""/>
+                                    <img className="img-fluid w-100" src="img/product-1.jpg" alt=""/>
                                     <div className="bg-secondary rounded text-white position-absolute start-0 top-0 m-4 py-1 px-3">New</div>
                                 </div>
                                 <div className="text-center p-4">
@@ -85,7 +108,7 @@ function Product() {
                         <div className="col-xl-3 col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.7s">
                             <div className="product-item">
                                 <div className="position-relative bg-light overflow-hidden">
-                                    <img className="img-fluid w-100" src="img/product-4.jpg" alt=""/>
+                                    <img className="img-fluid w-100" src="img/product-1.jpg" alt=""/>
                                     <div className="bg-secondary rounded text-white position-absolute start-0 top-0 m-4 py-1 px-3">New</div>
                                 </div>
                                 <div className="text-center p-4">
@@ -106,7 +129,7 @@ function Product() {
                         <div className="col-xl-3 col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
                             <div className="product-item">
                                 <div className="position-relative bg-light overflow-hidden">
-                                    <img className="img-fluid w-100" src="img/product-5.jpg" alt=""/>
+                                    <img className="img-fluid w-100" src="img/product-1.jpg" alt=""/>
                                     <div className="bg-secondary rounded text-white position-absolute start-0 top-0 m-4 py-1 px-3">New</div>
                                 </div>
                                 <div className="text-center p-4">
@@ -127,7 +150,7 @@ function Product() {
                         <div className="col-xl-3 col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
                             <div className="product-item">
                                 <div className="position-relative bg-light overflow-hidden">
-                                    <img className="img-fluid w-100" src="img/product-6.jpg" alt=""/>
+                                    <img className="img-fluid w-100" src="img/product-1.jpg" alt=""/>
                                     <div className="bg-secondary rounded text-white position-absolute start-0 top-0 m-4 py-1 px-3">New</div>
                                 </div>
                                 <div className="text-center p-4">
@@ -148,7 +171,7 @@ function Product() {
                         <div className="col-xl-3 col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
                             <div className="product-item">
                                 <div className="position-relative bg-light overflow-hidden">
-                                    <img className="img-fluid w-100" src="img/product-7.jpg" alt=""/>
+                                    <img className="img-fluid w-100" src="img/product-1.jpg" alt=""/>
                                     <div className="bg-secondary rounded text-white position-absolute start-0 top-0 m-4 py-1 px-3">New</div>
                                 </div>
                                 <div className="text-center p-4">
@@ -169,7 +192,7 @@ function Product() {
                         <div className="col-xl-3 col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.7s">
                             <div className="product-item">
                                 <div className="position-relative bg-light overflow-hidden">
-                                    <img className="img-fluid w-100" src="img/product-8.jpg" alt=""/>
+                                    <img className="img-fluid w-100" src="img/product-1.jpg" alt=""/>
                                     <div className="bg-secondary rounded text-white position-absolute start-0 top-0 m-4 py-1 px-3">New</div>
                                 </div>
                                 <div className="text-center p-4">
@@ -192,6 +215,8 @@ function Product() {
                         </div>
                     </div>
                 </div>
+
+
                 <div id="tab-2" className="tab-pane fade show p-0">
                     <div className="row g-4">
                         <div className="col-xl-3 col-lg-4 col-md-6">
