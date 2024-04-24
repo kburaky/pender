a2cf8074c0de475c4fbe806a5bd3856be5df5963
kburaky <kburak.ylmz@gmail.com>
2024-04-25 00:22:22 +0300

    new file:   public/img/kahvalti-olumlu-olumsuz-etki.jpg 	modified:   src/pages/ModelBilesenBes.jsx

--- src/pages/ModelBilesenBes.jsx
+++ src/pages/ModelBilesenBes.jsx
@@ -16,7 +16,7 @@ function ModelBilesenBes() {
       <div className="row g-5 align-items-center">
           <div className="col-lg-6 wow fadeIn" data-wow-delay="0.1s">
               <div className="about-img position-relative overflow-hidden p-5 pe-0">
-                  <img className="img-fluid w-100" alt="Olumlu ve Olumsuz Etki" src="img/about.jpg"/>
+                  <img className="img-fluid w-100" alt="Olumlu ve Olumsuz Etki" src="img/kahvalti-olumlu-olumsuz-etki.jpg"/>
               </div>
           </div>
           <div className="col-lg-6 wow fadeIn" data-wow-delay="0.5s">
@@ -29,6 +29,49 @@ function ModelBilesenBes() {
               
           </div>
       </div>
+
+      <div className="row g-5 align-items-center">
+                <div className="col-lg-6 wow fadeIn" data-wow-delay="0.1s">
+                <p>Çocuklar için düzenli kahvaltı yapmanın birçok faydası vardır:</p>
+<ol>
+<li><b>Enerji ve Odaklanma:</b> Kahvaltı yapmak, çocukların gün boyunca ihtiyaçları olan enerjiyi sağlar. Ayrıca kahvaltı, dikkatlerini toplamalarına ve okulda daha iyi performans göstermelerine yardımcı olabilir.</li>
+<li><b>	Besin Alımı:</b> Kahvaltı, birçok önemli besin grubunu içerebilir. Özellikle protein, lif, vitaminler ve mineraller açısından zengin bir kahvaltı, çocukların sağlıklı bir şekilde büyümelerine ve gelişmelerine yardımcı olabilir.</li>
+<li><b>	Kilo Kontrolü:</b> Düzenli kahvaltı yapmak, çocuklarda aşırı yeme eğilimini azaltabilir. Sabah kahvaltısı, metabolizmanın hızlanmasına ve gün boyunca daha uzun süre tok hissetmelerine yardımcı olabilir.</li>
+<li><b>	Zihinsel Gelişim:</b> Kahvaltı yapmak, çocukların zihinsel gelişimine katkıda bulunabilir. Özellikle omega-3 yağ asitleri gibi beyin için önemli besin maddelerini içeren kahvaltılar, bilişsel fonksiyonları destekleyebilir.</li>
+<li><b>	Dengeli Yaşam Tarzı Alışkanlığı:</b> Düzenli kahvaltı yapmak, çocuklara sağlıklı bir yaşam tarzı alışkanlığı kazandırabilir. Bu, ilerleyen yaşlarda obezite, diyabet ve diğer kronik hastalıkların riskini azaltabilir.</li>
+</ol>
+<p>Sonuç olarak, çocuklar için düzenli kahvaltı yapmanın faydaları, sağlıklı bir yaşam tarzının temellerini atmalarına ve günlük aktivitelerinde daha başarılı olmalarına yardımcı olabilir.</p>
+
+                </div>
+
+                <div className="col-lg-6 wow fadeIn" data-wow-delay="0.5s">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/quGM02_1Bms?si=RqrWBi1MAqDTQfYh" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                    
+                </div>
+      </div>
+<br/>
+      <div className="row g-5 align-items-center">
+                <div className="col-lg-6 wow fadeIn" data-wow-delay="0.1s">
+                <p>Çocukların düzenli kahvaltı yapmaması bir dizi olumsuz etkiye yol açabilir:</p>
+<ol>
+<li><b>Enerji Düzeylerinde Düşüş:</b> Kahvaltı yapmayan çocuklar, gün boyunca ihtiyaçları olan enerjiyi alamazlar. Bu durum, okulda dikkatlerinin dağılmasına, yorgunluğa ve halsizliğe neden olabilir.</li>
+<li><b>Öğrenme ve Akademik Performansta Azalma:</b> Düzenli kahvaltı yapmamanın, çocukların konsantrasyonunu ve öğrenme yeteneklerini olumsuz etkilediği gösterilmiştir. Bu durum, okul başarısında düşüşe neden olabilir.</li>
+<li><b>Besin Eksikliği: </b> Kahvaltı yapmamak, çocukların gün boyunca ihtiyaçları olan önemli besin maddelerini alamamalarına neden olabilir. Bu da büyüme, gelişme ve sağlıklı bir yaşam için gerekli olan besin eksikliklerine yol açabilir.</li>
+<li><b>Metabolizma Problemleri:</b> Kahvaltı yapmayan çocuklar, metabolizma hızlarının düşmesine ve vücutlarının enerjiyi daha etkili bir şekilde kullanamamasına neden olabilirler. Bu durum, kilo kontrolü problemlerine yol açabilir.</li>
+<li><b>Davranış Sorunları:</b> Açlık, çocuklarda irritabilite (sinirlilik), huysuzluk ve huzursuzluk gibi davranış sorunlarına yol açabilir. Bu da sosyal etkileşimlerini ve genel mutluluklarını etkileyebilir.</li>
+<li><b>Düzensiz Yeme Alışkanlıkları:</b> Düzenli kahvaltı yapmamanın alışkanlık haline gelmesi, çocuklarda daha sonra sağlıksız yeme alışkanlıklarına yol açabilir. Bu da obezite ve diğer sağlık sorunları riskini artırabilir.</li>
+</ol>
+<p>Sonuç olarak, çocukların düzenli kahvaltı yapmaması, genel sağlık ve iyi oluşları üzerinde olumsuz etkileri olan bir dizi zarara neden olabilir. Bu nedenle, sağlıklı bir yaşam tarzı için düzenli kahvaltı alışkanlığının teşvik edilmesi önemlidir.</p>
+
+                </div>
+
+                <div className="col-lg-6 wow fadeIn" data-wow-delay="0.5s">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/s9ZMRUShyxQ?si=VNU2_2qftiyLfHGX" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                    
+                </div>
+      </div>
+
+
   </div>
 </div>
 <Footer />
