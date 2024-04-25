9d6b1bb5144f404bb010c9e91b5aef01fcd7d70d
kburaky <kburak.ylmz@gmail.com>
2024-04-25 21:42:20 +0300

    new file:   public/img/algilanan-engeller.png 	modified:   src/pages/ModelBilesenUc.jsx

--- src/pages/ModelBilesenUc.jsx
+++ src/pages/ModelBilesenUc.jsx
@@ -16,7 +16,7 @@ function ModelBilesenUc() {
       <div className="row g-5 align-items-center">
           <div className="col-lg-6 wow fadeIn" data-wow-delay="0.1s">
               <div className="about-img position-relative overflow-hidden p-5 pe-0">
-                  <img className="img-fluid w-100" alt="Algılanan Engeller" src="img/about.jpg"/>
+                  <img className="img-fluid w-100" alt="Algılanan Engeller" src="img/algilanan-engeller.png"/>
               </div>
           </div>
           <div className="col-lg-6 wow fadeIn" data-wow-delay="0.5s">
@@ -31,6 +31,37 @@ function ModelBilesenUc() {
               
           </div>
       </div>
+
+      <div className="row g-5 align-items-center">
+                <div className="col-lg-6 wow fadeIn" data-wow-delay="0.1s">
+                
+                <p>Öğrencilerin kahvaltı yapmama sebepleri çeşitli olabilir:</p>
+
+              <ol>
+              <li><b>	Zaman Yetersizliği:</b> Sabahları okula yetişme telaşı içinde olan öğrenciler, kahvaltı için yeterli zamanı bulamayabilirler.</li>
+              <li><b>	Uykulu Olma:</b> Sabahları uyanmakta zorlanan öğrenciler, kahvaltı yapmak yerine uyumaya devam etmeyi tercih edebilirler.</li>
+              <li><b>	İştahsızlık:</b> Sabahları yemek yeme isteğinin düşük olması, öğrencileri kahvaltı yapmaktan alıkoyabilir.</li>
+              <li><b>	Evde Kahvaltı Alışkanlığının Olmaması:</b> Evde kahvaltı yapma alışkanlığının olmaması, öğrencileri dışarıda veya okulda kahvaltı etmeye teşvik edebilir.</li>
+              <li><b>	Maddi Durum:</b> Bazı öğrenciler, ekonomik sıkıntılar nedeniyle yeterli ve dengeli bir kahvaltı yapamayabilirler.</li>
+              <li><b>	Bilinçsiz Beslenme Alışkanlıkları:</b> Bilinçsizce beslenme alışkanlıkları, öğrencilerin sağlıklı kahvaltı seçimlerinden uzak durmalarına neden olabilir.</li>
+              <li><b>	Stres ve Endişe:</b> Sınav dönemleri veya önemli projelerle ilgili stres ve endişe, öğrencilerin iştahını azaltabilir ve dolayısıyla kahvaltı yapma isteğini azaltabilir.</li>
+              <li><b>	Diyet Uygulama:</b> Bazı öğrenciler, kilo verme veya vücutlarını şekillendirme amacıyla kahvaltı yapmamayı tercih edebilirler.</li>
+              <li><b>	Sosyal Etkileşim:</b> Bazı öğrenciler, sabahları kahvaltı yapmak yerine arkadaşlarıyla buluşmayı tercih edebilirler.</li>
+              <li><b>	Sağlıksız Alışkanlıklar:</b> Fast food alışkanlıkları, gazlı içecek tüketimi gibi sağlıksız alışkanlıklar, öğrencileri dengeli bir kahvaltı yapmaktan alıkoyabilir.</li>
+              </ol>
+
+
+                </div>
+                <div className="col-lg-6 wow fadeIn" data-wow-delay="0.5s">
+
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/5j-yRGwCJs4?si=1vdgKLY2_0dfNfap" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>    
+                    
+                </div>
+            </div>
+
+
+
+
   </div>
 </div>
 <Footer />
