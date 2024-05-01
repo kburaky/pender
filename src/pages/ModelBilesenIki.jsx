fb9fcbe766a88ce142a4f7728f83bab94ffc3cad
kburaky <kburak.ylmz@gmail.com>
2024-05-01 17:25:35 +0300

    new file:   public/img/algilanan-fayda.jpg 	modified:   src/pages/ModelBilesenIki.jsx

--- src/pages/ModelBilesenIki.jsx
+++ src/pages/ModelBilesenIki.jsx
@@ -16,7 +16,7 @@ function ModelBilesenIki() {
       <div className="row g-5 align-items-center">
           <div className="col-lg-6 wow fadeIn" data-wow-delay="0.1s">
               <div className="about-img position-relative overflow-hidden p-5 pe-0">
-                  <img className="img-fluid w-100" alt="Algılanan Fayda" src="img/about.jpg"/>
+                  <img className="img-fluid w-100" alt="Algılanan Fayda" src="img/algilanan-fayda.jpg"/>
               </div>
           </div>
           <div className="col-lg-6 wow fadeIn" data-wow-delay="0.5s">
@@ -30,6 +30,37 @@ function ModelBilesenIki() {
               
           </div>
       </div>
+
+      <div className="row g-5 align-items-center">
+        <div className="col-lg-6 wow fadeIn" data-wow-delay="0.1s">
+                
+              <p><b>Çocuklarda düzenli ve sağlıklı kahvaltı yapmanın algılanan faydaları şunlardır:</b></p>
+              
+              <ol>
+              <li><b>Bilişsel Performansın Artması:</b> Sağlıklı kahvaltı, çocukların okulda daha iyi performans göstermelerine yardımcı olabilir. Besin alımının sabah saatlerinde sağlanması, bilişsel fonksiyonları artırabilir, dikkatlerini toplamalarına ve öğrenmeye odaklanmalarına yardımcı olabilir.</li>
+              <li><b>Enerji Seviyelerinin Dengelemesi:</b> Sabah kahvaltısı, çocukların gün boyunca enerjik ve aktif kalmasını sağlar. Kahvaltı yapmayan çocuklar, gün içinde enerji düşüklüğü yaşayabilir ve öğrenme kapasiteleri etkilenebilir.</li>
+              <li><b>Duygusal Dengenin Sağlanması:</b> Sağlıklı kahvaltı, çocukların duygusal dengelerini korumalarına yardımcı olabilir. Kan şekerinin dengelenmesi ve tokluk hissinin sağlanması, duygusal dalgalanmaların önlenmesine katkıda bulunabilir.</li>
+              <li><b>Besin Alımının Dengelenmesi:</b> Sabah kahvaltısı, çocukların günlük besin ihtiyaçlarını karşılamalarına yardımcı olur. Kahvaltıda çeşitli besin gruplarına yer vermek, vitamin, mineral ve diğer besin öğelerinin yeterli miktarda alınmasını sağlar.</li>
+              <li><b>Obezite Riskinin Azalması:</b> Düzenli kahvaltı yapmak, çocukların obezite riskini azaltabilir. Sağlıklı ve dengeli bir kahvaltı, gün içinde aşırı yeme eğilimini azaltır ve uzun vadede sağlıklı kilo kontrolüne katkıda bulunur.</li>
+              <li><b>Sosyal ve Aile Bağlarının Güçlenmesi:</b> Ailelerin birlikte kahvaltı yapması, çocukların duygusal gelişimine olumlu katkıda bulunabilir. Sabah kahvaltısında aile bir araya gelerek birlikte vakit geçirmek, aile bağlarını güçlendirir ve çocukların kendilerini değerli hissetmelerini sağlar.</li>
+              <li><b>Davranış Problemlerinin Azalması:</b> Sağlıklı kahvaltı yapmanın, çocuklarda davranış problemlerini azaltıcı etkisi olduğuna dair bazı araştırmalar bulunmaktadır. Kan şekerinin dengelenmesi ve besin alımının sağlanması, çocukların daha sakin ve odaklanmış olmalarını sağlayabilir.</li>
+
+
+
+              </ol>
+              <p> Bu faydalar, çocukların sağlıklı büyüme ve gelişmelerini desteklemek için düzenli ve sağlıklı kahvaltı yapmalarının önemini vurgular.</p>
+        </div>
+        <div className="col-lg-6 wow fadeIn" data-wow-delay="0.5s">
+        
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/W6Q6OA2O-iQ?si=aS4Ociwtk6NGj_-p" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                   
+                    
+        </div>
+      </div>
+
+
+
+
   </div>
 </div>
 <Footer />
