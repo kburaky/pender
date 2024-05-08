f1d4a0613da1785500fb82e0c0a04909d07d8f8d
kburaky <kburak.ylmz@gmail.com>
2024-05-08 22:57:18 +0300

    modified:   package-lock.json 	modified:   package.json 	modified:   src/pages/Brochures.jsx

--- src/pages/Brochures.jsx
+++ src/pages/Brochures.jsx
@@ -4,6 +4,7 @@ import Breadcrumb from '../components/Breadcrumb'
 import NavBar from '../components/NavBar'
 import Footer from '../components/Footer'
 
+
 function Brochures() {
   return (
     <div>
@@ -13,9 +14,16 @@ function Brochures() {
       <div className="container-xxl py-5">
   <div className="container">
       <div className="row g-5 align-items-center">
-          <div className="col-lg-12 wow fadeIn" data-wow-delay="0.1s">
+          <div className="col-lg-6 wow fadeIn" data-wow-delay="0.1s">
           
           <img className="img-fluid w-100" alt='' src="img/bros1.png"/>
+
+
+          </div>
+
+          <div className="col-lg-6 wow fadeIn" data-wow-delay="0.1s">
+          
+          <img className="img-fluid w-100" alt='' src="img/bros2.png"/>
 
 
           </div>
@@ -23,9 +31,15 @@ function Brochures() {
       </div>
 
       <div className="row g-5 align-items-center">
-        <div className="col-lg-12 wow fadeIn" data-wow-delay="0.1s">
+        <div className="col-lg-6 wow fadeIn" data-wow-delay="0.1s">
                 
-        <img className="img-fluid w-100" alt='' src="img/bros2.png"/>
+        
+
+        </div>
+
+        <div className="col-lg-6 wow fadeIn" data-wow-delay="0.1s">
+                
+        
 
         </div>
         
